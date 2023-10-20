4bd829dcd405069ce3c2d6f26740388fcda0c23d
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-10-21 02:18:19 +0700

    s

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -21,10 +21,10 @@ export const HomeTemplate = () => {
         }}
         className="mySwiper"
       >
-        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px]  w-full' src="/public/images/dat-rung-phuong-nam.jpeg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px]  w-full' src="/public/images/doraemon.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px]  w-full' src="/public/images/Mission-Impossible-7.jpg" alt="" /></SwiperSlide>
-        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px]  w-full' src="/public/images/moving.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px] w-full' src="/images/dat-rung-phuong-nam.jpeg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px] w-full' src="/images/doraemon.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px] w-full' src="/images/Mission-Impossible-7.jpg" alt="" /></SwiperSlide>
+        <SwiperSlide><img className='h-[175px] 2xl:h-[880px] xl:h-[720px] lg:h-[520px] md:h-[375px] sm:h-[300px] w-full' src="/images/moving.jpg" alt="" /></SwiperSlide>
       </Swiper>
     </div>
   )
