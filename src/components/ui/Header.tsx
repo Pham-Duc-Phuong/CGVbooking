bfb9b3af2d8eceb1a9ae7389240742262a0129de
Pham Duc Phuong <phuong.mom33@gmail.com>
2023-12-31 20:45:38 +0700

    s

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -90,7 +90,7 @@ export const Header = () => {
 
 
                     </div>
-                    <div className={cn("items-center justify-between md:block phone:hidden w-full md:w-auto md:order-1")} id="navbar-user">
+                    <div className={"items-center justify-between md:block phone:hidden w-full md:w-auto md:order-1"} id="navbar-user">
                         <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
                                 <p className="p-header" onClick={() => {
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { rootReducers } from './rootReducer'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { ThongTinTaiKhoanThunk, loginThunk } from './quanLyNguoiDung'
+import { ThongTinTaiKhoanThunk } from './quanLyNguoiDung'
 
 
 export const store = configureStore({
     reducer: rootReducers
 })
-store.dispatch(loginThunk({ taiKhoan: 'phuongporo8123', matKhau: '123' }))
+
 store.dispatch(ThongTinTaiKhoanThunk())
 // UseState in TypeScript
 export type RootState = ReturnType<typeof store.getState>
